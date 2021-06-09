b33351c942fedca483bbb7e50e389d4408ca718a
p-alex <alexey9021@gmail.com>
2021-06-09 19:50:45 +0300

    Moved SectionContainer to component-library

--- containers/SectionContainer.js
+++
@@ -1,4 +0,0 @@
-import styles from "../styles/SectionContainer.module.css";
-export default function SectionContainer({ children }) {
-  return <section className={styles.container}>{children}</section>;
-}

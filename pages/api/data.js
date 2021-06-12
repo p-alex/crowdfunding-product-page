0862cc9575d79000d74840134de6ff7bd4c920f8
p-alex <alexey9021@gmail.com>
2021-06-13 00:11:16 +0300

    Added data api endpoint where i retrieve data from mongodb database

---
+++ pages/api/data.js
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
+  if (req.method === "GET") {
+    const client = await MongoClient.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    const db = client.db();
+    const collection = db.collection("MastercraftProject");
+    const data = await collection.find().toArray();
+    client.close();
+    res.json(data);
+  }
+}
+export default handler;
